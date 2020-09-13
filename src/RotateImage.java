f8a0bf23f28927cb0aa4f6312a999ea2b71e1885
Roshandeep <roshandeep810@gmail.com>
2020-09-12 22:15:38 -0400

    Daily Update

---
+++ src/RotateImage.java
@@ -0,0 +1,49 @@
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int row=matrix.length;
+
+        for(int i=0;i<row;i++){
+            for(int j=i;j<row;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        int start=0, end=matrix.length-1;
+        while(start<end){
+            for(int i=0;i<row;i++){
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+                matrix[i][end]=temp;
+            }
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][]=new int[4][4];
+        int count=1;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                matrix[i][j]=count;
+                count++;
+            }
+        }
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(matrix[i][j] + "  ");
+            }
+            System.out.println();
+        }
+        RotateImage obj=new RotateImage();
+        obj.rotate(matrix);
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(matrix[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
