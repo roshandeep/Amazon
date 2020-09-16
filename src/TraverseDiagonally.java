c364f3a61ded06e0a4ed666beb24678667fb2566
Roshandeep <roshandeep810@gmail.com>
2020-09-16 05:53:07 -0400

    Daily Update

--- src/TraverseDiagonally.java
+++ src/TraverseDiagonally.java
@@ -1,62 +1,57 @@
 public class TraverseDiagonally {
     public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix.length == 0) {
-            return new int[0];
-        }
+
         int row = matrix.length;
-        int column = matrix[0].length;
-        int[] diagonal = new int[row * column];
+        int col = matrix[0].length;
+        int[] diagonal = new int[row * col];
+        boolean upmove=true;
+        int rowindex=0,colindex=0;
+        for(int i=0;i<row*col;i++){
+            diagonal[i]=matrix[rowindex][colindex];
+            if(upmove){
+                if(colindex+1>=col){
+                    upmove=false;
+                    rowindex++;
+                }
+                else if(rowindex-1<0){
+                    upmove=false;
+                    colindex++;
+                }
+                else{
+                    rowindex--;
+                    colindex++;
+                }
+            }
+            else{
+                if(rowindex+1>=row){
+                    upmove=true;
+                    colindex++;
+                }
+                else if(colindex-1<0){
+                    upmove=true;
+                    rowindex++;
+                }
+                else{
+                    rowindex++;
+                    colindex--;
+                }
+            }
+        }
 
         return diagonal;
     }
 
     public static void main(String[] args) {
-        int arr[][] = {{1, 2, 3,4},
-                        {8, 9, 10, 5},
-                        {15, 16, 17, 6}};
+        int arr[][] = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
         TraverseDiagonally obj=new TraverseDiagonally();
         int diagonal[] = obj.findDiagonalOrder(arr);
         for (int i=0;i<diagonal.length;i++){
-            System.out.println(diagonal[i] + " ");
+            System.out.print(diagonal[i] + " ");
         }
     }
 }
-
-//    public int[] findDiagonalOrder(int[][] matrix) {
-//        if (matrix.length == 0) {
-//            return new int[0];
-//        }
-//        int row = matrix.length;
-//        int column = matrix[0].length;
-//        int[] result = new int[row * column];
-//        int x = 0;
-//        int currRow = 0, currCol = 0;
-//        boolean up = true; // otherwise, it's down
-//        while (x != (row * column)) {
-//            result[x++] = matrix[currRow][currCol];
-//            if (up) {
-//                if (currCol + 1 >= column) {
-//                    up = false;
-//                    currRow++;
-//                } else if (currRow - 1 < 0) {
-//                    up = false;
-//                    currCol++;
-//                } else {
-//                    currRow--;
-//                    currCol++;
-//                }
-//            } else {
-//                if (currRow + 1 >= row) {
-//                    up = true;
-//                    currCol++;
-//                } else if (currCol - 1 < 0) {
-//                    up = true;
-//                    currRow++;
-//                } else {
-//                    currRow++;
-//                    currCol--;
-//                }
-//            }
-//        }
-//        return result;
-//    }
